2f43626360ffb2dd5e318831bdb7c927e720bfa7
José LR Reis <jreis78pt@gmail.com>
2020-07-25 15:49:58 -0300

    Material Module test

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,13 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   //imports: [],
   exports: [
-
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
