75c7bc8db48ab76766fb944e1b8b46828c12f399
José LR Reis <jreis78pt@gmail.com>
2020-07-25 15:59:22 -0300

    Main components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { LoginComponent } from './auth/login/login.component';
 import { MaterialModule } from './material.module';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentTrainingComponent,
+    LoginComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent,
+    SignupComponent,
+    TrainingComponent,
   ],
   imports: [
     BrowserModule,
