e1c227668991c5a04cab8c8b7ed7c259635b4f59
José LR Reis <jreis78pt@gmail.com>
2020-07-25 16:22:39 -0300

    Signup input fields

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   //imports: [],
   exports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
   ]
 })
 export class MaterialModule { }
