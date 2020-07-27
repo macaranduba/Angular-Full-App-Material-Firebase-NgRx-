b6776a546f335f033442f3fb75b03d4342ad200d
José LR Reis <jreis78pt@gmail.com>
2020-07-27 17:29:10 -0300

    Datepicker restricting date value (minimal age)

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,10 +7,14 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  maxDate = undefined; // max birth date so that user must have, at least, 18 years old
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
 
   onSubmit(form: NgForm) {
     console.log("SignupComponent.onSubmit()", form);
