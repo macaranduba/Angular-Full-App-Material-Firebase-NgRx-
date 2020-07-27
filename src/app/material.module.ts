de0e01c759f4b2571d82c6bc58c7d4f2697280ae
José LR Reis <jreis78pt@gmail.com>
2020-07-27 18:19:18 -0300

    Checkbox

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 
@@ -11,9 +12,10 @@ import { MatNativeDateModule } from '@angular/material/core';
   //imports: [],
   exports: [
     MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule,
     MatDatepickerModule,
     MatNativeDateModule,
   ]
