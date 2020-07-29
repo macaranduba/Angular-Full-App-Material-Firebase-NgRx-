13ea04ac9575de706cd78aefb9f16f6f3794efce
José LR Reis <jreis78pt@gmail.com>
2020-07-29 10:34:23 -0300

    Sidenav styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'traning', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent },
 ];
 
 @NgModule({
