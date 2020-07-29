bb594057707a14c4d350a2a2287e6497d64e7b99
José LR Reis <jreis78pt@gmail.com>
2020-07-29 17:51:34 -0300

    Splitting the Navigation Into Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { HeaderComponent } from './navigation/header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { MaterialModule } from './material.module';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { TrainingComponent } from './training/training.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CurrentTrainingComponent,
+    HeaderComponent,
     LoginComponent,
     NewTrainingComponent,
     PastTrainingsComponent,
     SignupComponent,
     TrainingComponent,
+    SidenavListComponent,
     WelcomeComponent,
   ],
   imports: [
