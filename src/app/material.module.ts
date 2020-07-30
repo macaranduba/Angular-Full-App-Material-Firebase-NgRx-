01f80b470ae3a2928061499b0b5398994b8d91a6
José LR Reis <jreis78pt@gmail.com>
2020-07-30 10:27:29 -0300

    Spinner (material design)

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -25,6 +26,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatInputModule,
     MatDatepickerModule,
     MatListModule,
+    MatProgressSpinnerModule,
     MatNativeDateModule,
     MatSelectModule,
     MatSidenavModule,
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.css']
 })
 export class TrainingComponent implements OnInit {
+  onGoingTraning = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
