fd247aa174c3937f81e270be0992cb575fccccf0
José LR Reis <jreis78pt@gmail.com>
2020-07-30 10:43:48 -0300

    Timer

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
-export class CurrentTrainingComponent implements OnInit {
+export class CurrentTrainingComponent implements OnInit, OnDestroy {
   progress = 0;
+  timerId: number;
 
   constructor() { }
 
   ngOnInit(): void {
-    setInterval( () => {
-      this.progress += 5; // 5% done per second
+    this.timerId = setInterval( () => {
+      this.progress += 20; // increment percentage done per second
+      if( this.progress >= 100 ) {
+        clearInterval( this.timerId );
+      }
     }, 1000);
+  }
+
+  onStopTimer() {
+    if ( this.timerId ) {
+      clearInterval( this.timerId );
+    }
+  }
+
+  ngOnDestroy() {
+    this.onStopTimer();
   }
 }
