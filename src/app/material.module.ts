150ac06f1cd39b5eed3674fe80e561e2db55cdaa
José LR Reis <jreis78pt@gmail.com>
2020-07-30 09:49:16 -0300

    Cards (material design)

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,6 +17,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   //imports: [],
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatCheckboxModule,
     MatFormFieldModule,
     MatIconModule,
