e115bb753f43209b95f0f8ca63a56b469411950a
José LR Reis <jreis78pt@gmail.com>
2020-08-03 11:20:43 -0300

    Firestore snapshot changes

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,10 +1,11 @@
+import { AngularFirestore, DocumentChangeType } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from './../training.service';
-import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-new-training',
@@ -18,7 +19,27 @@ export class NewTrainingComponent implements OnInit {
               private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+		this.exercises = this.db.collection('availableExercises').snapshotChanges()
+			.pipe(
+				map( docArray => {
+          console.info(docArray);
+					return docArray.map( doc => {
+						return {
+							id: doc.payload.doc.id,
+							... doc.payload.doc.data() as Exercise
+						};
+					});
+				})
+			)
+      /*.subscribe( result => {
+        console.log( result);
+      });*/
+		/* shorten alternative to get id filed mapped within the data object
+		    this.db
+          .collection('availableExercises')
+          .valueChanges({ idField: 'id' })
+          .subscribe((e : Exercise[]) => console.log(e));
+		*/
   }
 
   onStartTraining(f: NgForm) {
