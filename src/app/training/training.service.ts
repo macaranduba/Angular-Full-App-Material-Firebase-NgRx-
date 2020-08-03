2100b29cb2a82cf7125f119c8bef5938940f45ce
José LR Reis <jreis78pt@gmail.com>
2020-08-03 15:17:44 -0300

    Firestore snapshot restructure

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,20 +1,39 @@
-import { Subject } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { Subject, Observable } from 'rxjs';
 
 import { Exercise } from "./exercise.model";
 
+@Injectable()
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
-  private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Abdominais', duration: 30, calories: 8 },
-    { id: 'Touch Toes', name: 'Tocar dedos dos pés', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Flexão-Burpee ', duration: 60, calories: 8 }
-  ];
+  private availableExercises: Exercise[] = [];
   private runningExercise: Exercise;
   private exercises: Exercise[] = [];
 
-  getAvailableExercises() {
-    return this.availableExercises.slice(); // returns a copy of the array, so that external code won't change this internal reference
+  constructor(private db: AngularFirestore) {}
+
+  fetchAvailableExercises(): Observable<Exercise[]> {
+    return this.db.collection('availableExercises').snapshotChanges()
+			.pipe(
+				map( docArray => {
+          console.info(docArray);
+					return docArray.map( doc => {
+						return {
+							id: doc.payload.doc.id,
+							... doc.payload.doc.data() as Exercise
+						};
+					});
+        }),
+        tap( exercises => this.availableExercises = exercises),
+			);
+		/* shorten alternative to get id filed mapped within the data object
+		    this.db
+          .collection('availableExercises')
+          .valueChanges({ idField: 'id' })
+          .subscribe((e : Exercise[]) => console.log(e));
+		*/
   }
 
   startExercise(exerciseId: string) {
