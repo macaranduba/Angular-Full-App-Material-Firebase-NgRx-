052d78527f991247859999f649e9be09c4906658
José LR Reis <jreis78pt@gmail.com>
2020-08-06 10:35:30 -0300

    Spinner for the new training page

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,8 +2,8 @@ import { AngularFirestore, DocumentChangeType } from '@angular/fire/firestore';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
-import { Exercise } from '../exercise.model';
 import { TrainingService } from './../training.service';
 
 @Component({
@@ -13,12 +13,17 @@ import { TrainingService } from './../training.service';
 })
 export class NewTrainingComponent implements OnInit {
   exercises: Observable<any>;
+  isLoading = false;
 
   constructor(private trainingService: TrainingService,
               private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.exercises = this.trainingService.fetchAvailableExercises();
+    this.isLoading = true;
+    this.exercises = this.trainingService.fetchAvailableExercises()
+      .pipe(
+        tap( () => this.isLoading = false)
+      );
   }
 
   onStartTraining(f: NgForm) {
